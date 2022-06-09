e9d09da91cc5e946b10c91094cc370d7d65bab6f
Emma Forslund <emfo2102@student.miun.se>
2022-06-09 17:58:34 +0200

    Skapat sidan för bokning av frukost

---
+++ page-boka-frukost.php
@@ -0,0 +1,28 @@
+<?php
+get_header();
+?>
+<main class="page">
+    <ul class="breadcrumbs">
+        <?php
+        if (is_active_sidebar('breadcrumbs')) : ?>
+            <?php dynamic_sidebar('breadcrumbs'); ?>
+    </ul>
+<?php
+        endif;
+?>
+<section class="page-info">
+    <h1><?php the_title(); ?></h1>
+            <?php
+            the_content();
+            ?>
+            <?php
+            //Kontroll för att se om det finns bild
+            if (has_post_thumbnail()) {
+                the_post_thumbnail();
+            }
+            ?>
+    </article>
+</section>
+</main>
+<!--Här slutar huvudinnehåll och footer börjar-->
+<?php get_footer(); ?>
