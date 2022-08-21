b19ea08caeb0a7d013232afac5688febf7a91237
Emma Forslund <emfo2102@student.miun.se>
2022-08-22 01:15:02 +0200

    Gömmer bokningsformuläret när menyn klickas på

--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,8 @@
 /*
  * @Author: Emma Forslund - emfo2102 
  * @Date: 2022-06-16 13:44:52 
- * @Last Modified by:   Emma Forslund - emfo2102 
- * @Last Modified time: 2022-06-16 13:44:52 
+ * @Last Modified by: Emma Forslund - emfo2102
+ * @Last Modified time: 2022-08-22 01:14:02
  */
 
 "use strict";
@@ -15,8 +15,14 @@ window.onload = init;
 
 function init() {
 
-  //Funktion för hamburger-menyn
+  //Funktion för hamburger-menyn. 
   hamburger.addEventListener("click", () => {
+    //Visar menyn vid klick på hamburger-ikonen
     navUl.classList.toggle("show");
+    //Gömmer bokningsformuläret på boka-restaurang-sidan
+    let datepickerEl = document.getElementsByClassName("ui-datepicker")
+    for (let i = 0; i < datepickerEl.length; i++) {
+      datepickerEl[i].style.display = "none";
+    }
   })
 }
