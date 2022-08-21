692da99851bd89cb2e893d8cb39de58dfa6075b9
Emma Forslund <emfo2102@student.miun.se>
2022-08-22 00:21:18 +0200

    Sociala medier-widget area, form och medarbetare

--- page-om-kraft.php
+++ page-om-kraft.php
@@ -2,13 +2,13 @@
 /*
  * @Author: Emma Forslund - emfo2102 
  * @Date: 2022-06-16 13:48:43 
- * @Last Modified by:   Emma Forslund - emfo2102 
- * @Last Modified time: 2022-06-16 13:48:43 
+ * @Last Modified by: Emma Forslund - emfo2102
+ * @Last Modified time: 2022-06-16 15:51:33
  */
 get_header();
 //Array
 $arguments = array(
-    'category_name' => 'om-kraft',
+    'category_name' => 'Info',
 );
 
 //WP-query
@@ -16,13 +16,15 @@ $wpquery = new WP_Query($arguments);
 ?>
 
 <main>
-    <ul class="breadcrumbs">
-        <?php
-        if (is_active_sidebar('breadcrumbs')) : ?>
-            <?php dynamic_sidebar('breadcrumbs'); ?>
-    </ul>
+    <div class="page-breadcrumbs">
+        <ul class="breadcrumbs">
+            <?php
+            if (is_active_sidebar('breadcrumbs')) : ?>
+                <?php dynamic_sidebar('breadcrumbs'); ?>
+        </ul>
+    </div>
 <?php
-        endif;
+            endif;
 ?>
 <section class="page-info">
     <h1><?php the_title(); ?></h1>
@@ -44,7 +46,7 @@ $wpquery = new WP_Query($arguments);
         ?>
     </div>
 </section>
-<section class="dark-about">
+<div class="dark-about">
     <?php
     //Array
     $args = array(
@@ -69,14 +71,68 @@ $wpquery = new WP_Query($arguments);
                         <?php
                         //Kontroll för att se om det finns bild och skriver ut
                         if (has_post_thumbnail()) {
-                            the_post_thumbnail('hotel-room-small');
+                            the_post_thumbnail('breakfast-big');
                         } ?>
                     </div>
+                </div>
             </article>
     <?php
         }
     }
     ?>
+</div>
+<div class="flex-contact">
+    <!--Här infogas ett kontaktformulär-->
+    <div class="contact-form">
+        <h2>Skriv ett meddelande till oss</h2>
+        <?php
+        //Skriver ut ett formulär
+        echo do_shortcode('[ninja_form id=2]');
+        ?>
+    </div>
+    <div class="socialmedia">
+        <h2>Följ oss på sociala medier</h2>
+        <ul>
+            <?php
+            if (is_active_sidebar('sociala-medier')) : ?>
+                <?php dynamic_sidebar('sociala-medier'); ?>
+            <?php
+            endif;
+            ?>
+        </ul>
+
+    </div>
+</div>
+<section class="medarbetare">
+    <h2>Våra medarbetare</h2>
+    <div class="flex-personal">
+        <?php
+        //Array
+        $argss = array(
+            'category_name' => 'personal',
+        );
+
+        //WP-query
+        $wpquery2 = new WP_Query($argss);
+        ?>
+        <?php
+        if ($wpquery2->have_posts()) {
+            while ($wpquery2->have_posts()) {
+                $wpquery2->the_post();
+        ?>
+                <div>
+                    <?php
+                    //Kontroll för att se om det finns bild och skriver ut
+                    if (has_post_thumbnail()) {
+                        the_post_thumbnail('personal-picture');
+                    } ?>
+                    <h3><?php the_title(); ?></h3>
+                    <?php the_content(); ?>
+                </div>
+        <?php
+            }
+        }
+        ?>
     </div>
 </section>
 </main>
